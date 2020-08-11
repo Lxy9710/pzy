ed7409a32617bb6296002beedd8671cd568f82fc
claireliang <claire_stu@163.com>
2020-08-12 06:43:31 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49349" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52545" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -388,4 +388,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["C:/Users/clairelxy/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/index.js.map
+//# sourceMappingURL=index.js.map
